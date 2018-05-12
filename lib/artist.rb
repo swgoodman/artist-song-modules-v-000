9f5c0d5ec11ebc84c6763aad63ab967f6521d1f7
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-12 00:04:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,10 +9,6 @@ class Artist
 
   @@artists = []
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def initialize
     @@artists << self
     @songs = []
@@ -21,14 +17,6 @@ class Artist
   def self.all
     @@artists
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   @@artists.count
-  # end
 
   def add_song(song)
     @songs << song
